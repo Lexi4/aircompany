0eb07d82799e1817cce643309f613bd3f0a31d58
Alexey Sveshnikov <mr.alyxey13@gmail.com>
2020-04-04 19:16:37 +0300

    Complete java side refactoring task, for course: Clean Code

--- Java/src/main/java/plane/ExperimentalPlane.java
+++ Java/src/main/java/plane/ExperimentalPlane.java
@@ -1,16 +1,16 @@
-package Planes;
+package plane;
 
-import models.ClassificationLevel;
-import models.ExperimentalTypes;
+import model.ClassificationLevel;
+import model.ExperimentalType;
 
-public class experimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane{
 
-    private ExperimentalTypes type;
+    private ExperimentalType experimentalType;
     private ClassificationLevel classificationLevel;
 
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalType experimentalType, ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+        this.experimentalType = experimentalType;
         this.classificationLevel = classificationLevel;
     }
 
@@ -23,8 +23,8 @@ public class experimentalPlane extends Plane{
     }
 
     @Override
-    public boolean equals(Object o) {
-        return super.equals(o);
+    public boolean equals(Object planeObject) {
+        return super.equals(planeObject);
     }
 
     @Override
@@ -34,8 +34,6 @@ public class experimentalPlane extends Plane{
 
     @Override
     public String toString() {
-        return "experimentalPlane{" +
-                "model='" + model + '\'' +
-                '}';
+        return "ExperimentalPlane{model='" + model + "'}";
     }
 }
